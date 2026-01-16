153c6dd868caa731a3ff6f5ed8300f2224e9cd58
leejy1685 <ucc1685@naver.com>
2026-01-16 15:22:28 +0900

    Fix : 어드레서블도 UniTask로 변경

--- Assets/01.Scripts/Global/Managers/UIPrefabManager.cs
+++ Assets/01.Scripts/Global/Managers/UIPrefabManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
@@ -16,58 +18,92 @@ public enum UIPrefabs
 /// </summary>
 public class UIPrefabManager : MonoBehaviour
 {
-    public static UIPrefabManager Instance { get; private set; }
+    private static UIPrefabManager _instance;
+
+    public static UIPrefabManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<UIPrefabManager>();
+
+                if (_instance != null)
+                {
+                    DontDestroyOnLoad(_instance.gameObject);
+                }
+            }
+
+            return _instance;
+        }
+    }
     
-    private Dictionary<GameObject, AsyncOperationHandle<GameObject>> activePrefabHandles = 
+    private Transform _currentMainCanvas;
+    
+    private Dictionary<GameObject, AsyncOperationHandle<GameObject>> _activePrefabHandles = 
         new Dictionary<GameObject, AsyncOperationHandle<GameObject>>();
 
     private void Awake()
     {
-        if (Instance == null)
+        // 인스턴스가 아직 설정되지 않았거나, 현재 인스턴스가 나라면
+        if (_instance == null || _instance == this)
         {
-            Instance = this;
+            _instance = this;
             DontDestroyOnLoad(gameObject);
         }
-        else
+        else // 이미 다른 인스턴스가 존재하면
         {
-            Destroy(gameObject);
+            Destroy(gameObject); // 나는 파괴
         }
+
+    }
+
+    public void RegisterMainCanvas(Transform canvas)
+    {
+        _currentMainCanvas = canvas;
+    }
+
+    public void UnregisterMainCanvas()
+    {
+        _currentMainCanvas = null;
     }
 
 
-    public async Task<GameObject> ShowUI(UIPrefabs address)
+    public async UniTask<GameObject> ShowUI(UIPrefabs address)
     {
-        AsyncOperationHandle<GameObject> loadHandle = ResourceLoader.LoadAssetAsync<GameObject>(address.ToString());
-        
-        await loadHandle.Task;
-
-        if (loadHandle.Status == AsyncOperationStatus.Succeeded)
+        AsyncOperationHandle<GameObject> loadHandle = default;
+        try
         {
-            GameObject prefabAsset = loadHandle.Result;
-            Transform uiParent = FindObjectOfType<Canvas>().transform;
-            GameObject instance = ObjectPool.Get(prefabAsset, uiParent);
+            if (_currentMainCanvas == null)
+                throw new InvalidOperationException("UI 캔버스 없음.");
             
-            activePrefabHandles.Add(instance, loadHandle);
+            loadHandle = ResourceLoader.LoadAssetAsync<GameObject>(address.ToString());
+            GameObject prefabAsset = await loadHandle;
+            
+            GameObject instance = ObjectPool.Get(prefabAsset, _currentMainCanvas);
+            _activePrefabHandles.Add(instance, loadHandle);
             
             return instance;
         }
-        else
+        catch (Exception e)
         {
-            ResourceLoader.Release(loadHandle);
-            
+            Debug.LogError($"UI '{address}' 로드 실패: {e.Message}");
+            if (loadHandle.IsValid())
+            {
+                ResourceLoader.Release(loadHandle);
+            }
             return null;
         }
+        
     }
     
     public void CloseUI(GameObject instance)
     {
-        if (activePrefabHandles.TryGetValue(instance, out AsyncOperationHandle<GameObject> handle))
+        if (_activePrefabHandles.TryGetValue(instance, out AsyncOperationHandle<GameObject> handle))
         {
             ObjectPool.Release(instance);
-            
             ResourceLoader.Release(handle); 
-            
-            activePrefabHandles.Remove(instance);
+            _activePrefabHandles.Remove(instance);
         }
         else
         {
